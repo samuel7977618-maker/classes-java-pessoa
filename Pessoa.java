a3789ef944bedc81f9b256f24dba362c6351805d
samuel7977618-maker <samuel.7977618@aluno.mg.gov.br>
2026-04-15 10:20:12 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,12 @@
+_______________________________________________________package Testes;
+public class Pessoa {
+String nome;
+int idade;
+        public void caminhar() {
+        System.out.println("caminhando...");
+        }
+        public void dormir() {
+        System.out.println("dormir...");
+        }
+}
+________________________________________________________
